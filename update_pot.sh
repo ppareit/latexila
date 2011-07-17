1e013a86234ec3ef593201e2068d38982b0a8255
Sébastien Wilmet <swilmet@src.gnome.org>
2011-07-17 14:18:26 +0200

    [l10n] Update latexila.pot

--- update_pot.sh
+++ update_pot.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-xgettext -k_ -kN_ -d latexila -s -o po/sources.pot src/*.vala
+xgettext -k_ -kN_ -d latexila --from-code=UTF-8 -o po/sources.pot src/*.vala
 xgettext -o po/glade.pot --language=Glade --omit-header src/ui/*.ui
 cd po/
 msgcat -o latexila.pot --use-first sources.pot glade.pot
