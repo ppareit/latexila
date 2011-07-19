c24511bba7b74b0c1c14dcc28ac27d1dd19645a2
Sébastien Wilmet <swilmet@src.gnome.org>
2011-07-19 22:58:46 +0200

    Translations: use intltool-update instead of custom script

--- update_pot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-xgettext -k_ -kN_ -d latexila --from-code=UTF-8 -o po/sources.pot src/*.vala
-xgettext -o po/glade.pot --language=Glade --omit-header src/ui/*.ui
-cd po/
-msgcat -o latexila.pot --use-first sources.pot glade.pot
-rm sources.pot glade.pot
