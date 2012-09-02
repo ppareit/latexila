f5eb45611dee6fff5f4f606c10d3dfb45f7f144a
Sébastien Wilmet <swilmet@gnome.org>
2012-09-03 00:00:16 +0200

    Autotools: the po/ directory
    It requires a more elaborated autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
+# Run this to generate all the initial makefiles, etc.
 
-mkdir -p m4
-autoreconf -i
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd "$srcdir"
+
+INTLTOOLIZE=`which intltoolize`
+if test -z $INTLTOOLIZE; then
+        echo "*** No intltoolize found, please install the intltool package ***"
+        exit 1
+fi
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found, please install it ***"
+        exit 1
+fi
+
+if test -z `which autopoint`; then
+        echo "*** No autopoint found, please install it ***"
+        exit 1
+fi
+
+autopoint --force || exit $?
+AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install --verbose
+
+cd "$olddir"
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
