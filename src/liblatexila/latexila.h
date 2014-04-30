43d7ca14fe01b3ef58dd5897e61eade179519464
Sébastien Wilmet <swilmet@gnome.org>
2014-04-30 17:11:45 +0200

    latexila.h to ease introspection support

---
+++ src/liblatexila/latexila.h
@@ -0,0 +1,29 @@
+/*
+ * This file is part of LaTeXila.
+ *
+ * Copyright (C) 2014 - Sébastien Wilmet <swilmet@gnome.org>
+ *
+ * LaTeXila is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LaTeXila is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LaTeXila.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LATEXILA_H__
+#define __LATEXILA_H__
+
+#include "latexila-build-tools.h"
+#include "latexila-build-tools-default.h"
+#include "latexila-build-tools-personal.h"
+#include "latexila-post-processor.h"
+#include "latexila-post-processor-all-output.h"
+
+#endif /* __LATEXILA_H__ */
